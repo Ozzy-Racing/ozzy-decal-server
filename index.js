3399e9f0e37054bd522031294f33f0356ec74f42
Adam Greenfield-Osgood <race@ozzyracing.co.uk>
2026-08-16 16:16:08 +0100

    Test: increase colour precision by one step

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.post('/trace', async (req, res) => {
 
     const buffer = Buffer.from(imageBase64, 'base64');
 
-    const colorPrecision = detailMode === 'fine' ? 7 : 6;
+    const colorPrecision = detailMode === 'fine' ? 8 : 7;
     const filterSpeckle = detailMode === 'fine' ? 4 : 5;
 
     const svg = await vectorize(buffer, {
